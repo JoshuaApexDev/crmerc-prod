162fb84312d4fac768a1fdee05fa5d32f8e5c4f7
Josue <jmmojica5@gmail.com>
2023-07-21 07:48:04 -0700

    Third push and fix for email

--- resources/views/mail/securelink.blade.php
+++ resources/views/mail/securelink.blade.php
@@ -76,7 +76,7 @@
                                     <td class="content-cell" align="center"
                                         style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; padding: 35px;">
                                         <p style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #aeaeae; font-size: 12px; text-align: center;">
-                                            © 2023 ApexDevelopment. All rights reserved.</p>
+                                            © 2023 All rights reserved.</p>
                                     </td>
                                 </tr>
                             </table>
